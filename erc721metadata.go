5f1380884a3b9f23887fe133762d5e98d4aaa9f1
Ryo Manzoku <manzoku.ryo@gmail.com>
2020-01-19 23:54:25 +0000

    Add error handling

--- erc721metadata.go
+++ erc721metadata.go
@@ -52,11 +52,11 @@ type RareBitsProperty struct {
 func FetchERC721Metadata(tokenURI string) (*ERC721Metadata, error) {
 	client := new(http.Client)
 	req, err := http.NewRequest("GET", tokenURI, nil)
-	req.Header.Add("Content-Type", "application/json")
-	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := client.Do(req)
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
